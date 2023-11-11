5f55b2c51064501c259a7c368fd2942a78027fcf
Tyler Pixley <tylerpixley@gmail.com>
2023-11-11 21:42:00 +0100

    attempting use of freebind #1

--- vban.py
+++ vban.py
@@ -17,6 +17,7 @@ class VBAN_Recv(object):
 			try:
 				self.sock = socket.socket(socket.AF_INET6 if ipv6 else socket.AF_INET, socket.SOCK_DGRAM) # UDP
 				self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+				self.sock.setsockopt(socket.SOL_IP, IP_FREEBIND, 1)
 				self.sock.bind(socketAddr)
 			except Exception as e:
 				print(e)
